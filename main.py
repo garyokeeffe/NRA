74885f4e55116c4cbc34ada40b2a8ca38af6409a
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-24 21:20:54 -0700

    label nostr data with openai's embeddings

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from nsa_wrapper import download_unfiltered_nostr_data
+from openai_wrapper import embedding_annotate
 from organization_helpers import save, load
 
+'''
 notes = download_unfiltered_nostr_data(1000)
 
 save(notes, "notes")
 #notes = load("notes")
+
+notes_with_embeddings = embedding_annotate(notes)
+
+save(notes_with_embeddings, "notes_with_embeddings")
+'''
+notes_with_embeddings = load("notes_with_embeddings")
+
+
+
+
+
