e6ce368ad0b411cbce4393685d8ac0f4be3455a1
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-25 19:49:09 -0700

    add chromadb compatibility

---
+++ chromadb_wrapper.py
@@ -0,0 +1,30 @@
+import chromadb
+from openai_wrapper import get_embedding
+
+def set_up_database():
+    client = chromadb.Client()
+    return client.create_collection("nostr_data")
+
+def upload_embeddings_to_database(database, notes_with_embeddings):
+    for note in notes_with_embeddings:
+        database.add(
+            documents = [notes_with_embeddings[note]['content']],
+            metadatas=[{
+	                'author': notes_with_embeddings[note]['author'],
+	                'time_created': notes_with_embeddings[note]['time_created']
+	                }],
+            ids=[note], 
+            embeddings = notes_with_embeddings[note]['embedding']
+        )
+    return database
+   
+
+def query_database_string(database, text, n_results = 5):
+    embedding = get_embedding(text)
+    return database.query(
+        query_embeddings=[embedding],
+        n_results=n_results,
+        # where={"metadata_field": "is_equal_to_this"}, # optional filter
+        # where_document={"$contains":"search_string"}  # optional filter
+    )  
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from nsa_wrapper import download_unfiltered_nostr_data
 from openai_wrapper import embedding_annotate
 from organization_helpers import save, load
+from chromadb_wrapper import set_up_database, upload_embeddings_to_database, query_database_string
 
 '''
 notes = download_unfiltered_nostr_data(1000)
@@ -14,7 +15,16 @@ save(notes_with_embeddings, "notes_with_embeddings")
 '''
 notes_with_embeddings = load("notes_with_embeddings")
 
+database = set_up_database()
+
+database = upload_embeddings_to_database(database, notes_with_embeddings)
 
 
+while True:
+	query_text = input("Enter your nostr text query (or 'exit' to stop): ")
+	if query_text.lower() == 'exit':
+		break
 
+	query_result = query_database_string(database, query_text)
+	print(query_result['documents'])
 
