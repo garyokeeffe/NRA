4be77845e892484f8c6c4e4a0151b70460294716
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-26 20:51:25 -0700

    handle media types and nostr events

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from nsa_wrapper import download_unfiltered_nostr_data
 from openai_wrapper import embedding_annotate
-from organization_helpers import save, load
-from chromadb_wrapper import set_up_database, upload_embeddings_to_database, query_database_string
+from organization_helpers import save, load, initial_semantic_focus, filter_long_and_short_notes, tag_media_notes, tag_nostr_notes
+from chromadb_wrapper import set_up_database, upload_embeddings_to_database, query_database_string, query_database_embedding
+
+
 
 '''
-notes = download_unfiltered_nostr_data(1000)
+notes = download_unfiltered_nostr_data(20000)
 
 save(notes, "notes")
-#notes = load("notes")
+'''
+notes = load("notes")
+
+
+tag_media_notes(notes)
+tag_nostr_notes(notes)
+filter_long_and_short_notes(notes)
+
+print(len(notes))
 
 notes_with_embeddings = embedding_annotate(notes)
 
-save(notes_with_embeddings, "notes_with_embeddings")
+#save(notes_with_embeddings, "notes_with_embeddings")
+
 '''
 notes_with_embeddings = load("notes_with_embeddings")
+'''
 
 database = set_up_database()
 
 database = upload_embeddings_to_database(database, notes_with_embeddings)
 
 semantic_focus = initial_semantic_focus('npub10mgeum509kmlayzuvxhkl337zuh4x2knre8ak2uqhpcra80jdttqqvehf6')
-query_result = query_database_embedding(database, semantic_focus,n_results=10)
+query_result = query_database_embedding(database, semantic_focus,n_results=5)
 print(query_result['documents'])
 '''
 while True:
@@ -31,3 +43,4 @@ while True:
 	query_result = query_database_string(database, query_text)
 	print(query_result['documents'])
 
+'''
--- organization_helpers.py
+++ organization_helpers.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 from nsa_wrapper import download_author_notes
 from openai_wrapper import embedding_annotate
+import re
+
 
 def save(data_object, name):
     with open(f'{name}.json', 'w') as f:
@@ -17,9 +19,46 @@ def get_embeddings_array(notes_with_embeddings):
     return np.array(embeddings)
 
 def initial_semantic_focus(author_pubkey):
-    notes = download_author_notes(author_pubkey, 2)
+    notes = download_author_notes(author_pubkey, 20)
+    filter_media_notes(notes)
+    filter_nostr_notes(notes)
     embedded_notes = embedding_annotate(notes)
     embeddings = get_embeddings_array(embedded_notes)
     return embeddings.mean(axis=0).tolist()
 
-def filter_notes(notes):
+
+def filter_long_and_short_notes(notes):
+    max_text_length = 1000
+    min_text_length = 20
+    keys_to_delete = [k for k, v in notes.items() if len(v['content']) > max_text_length]
+    for key in keys_to_delete:
+        del notes[key]
+    keys_to_delete = [k for k, v in notes.items() if len(v['content']) < min_text_length]
+    for key in keys_to_delete:
+        del notes[key]
+    return notes
+
+
+
+def filter_media_notes(notes):
+    media_types = [".gif", "https", "youtube.com", ".png", ".jpg", ".webp", "spotify", ".mov"]
+
+    url_pattern = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+
+    for k, v in notes.items():
+        urls = re.findall(url_pattern, v['content'])
+        for url in urls:
+            if any(type in url[0] for type in media_types):
+                v['content'] = v['content'].replace(url[0], ' [media_object] ')
+
+    return notes
+
+def filter_nostr_notes(notes):
+    media_types = ["nostr:"]
+
+    for k, v in notes.items():
+        for media_type in media_types:
+            pattern = re.escape(media_type) + r'\S*'
+            v['content'] = re.sub(pattern, '[nostr_object]', v['content'])
+
+    return notes
